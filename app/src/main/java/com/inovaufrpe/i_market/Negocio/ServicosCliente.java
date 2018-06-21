97d48b10a91c898f4db3374c91dff2509ba23c8c
WendersonLeonardo <32573898+WendersonLeonardo@users.noreply.github.com>
2018-06-21 15:07:16 -0300

    Update ServicosCliente.java

--- app/src/main/java/com/inovaufrpe/i_market/Negocio/ServicosCliente.java
+++ app/src/main/java/com/inovaufrpe/i_market/Negocio/ServicosCliente.java
@@ -3,6 +3,7 @@ package com.inovaufrpe.i_market.Negocio;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.inovaufrpe.i_market.Dominio.Cliente;
+import com.inovaufrpe.i_market.Dominio.Usuario;
 
 import java.util.UUID;
 
@@ -15,16 +16,22 @@ public class ServicosCliente {
         databaseReference = firebaseDatabase.getReference();
     }
 
-    public void cadastrarCliente(String id_usuario, String nome, String endereco, long cpf, int num_telefone,  int num_cartao){
+    public void cadastrarCliente(String email, String senha, String nick, String nome, String endereco, long cpf, int num_telefone,  int num_cartao){
+        ServicosUsuario servicosUsuario = new ServicosUsuario();
+        Usuario usuario = new Usuario();
+        usuario.setUid(UUID.randomUUID().toString());
+        servicosUsuario.cadastrarUsuario(usuario,email, senha, nick);
+
         Cliente cliente = new Cliente();
 
         cliente.setId(UUID.randomUUID().toString());
+        cliente.setUid_usuario(usuario.getUid());
         cliente.setNome(nome);
         cliente.setEndereco(endereco);
         cliente.setCpf(cpf);
         cliente.setNum_telefone(num_telefone);
         cliente.setNum_cartao(num_cartao);
-
+        
         this.insertCliente(cliente);
     }
 
